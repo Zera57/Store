686c2d1cb55e64a562277c51c8defad803380e16
Zera57 <svetk.avtonomov@gmail.com>
2023-06-11 10:32:07 +0300

    Maked security part

--- src/main/kotlin/com/zera/store/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/zera/store/security/WebSecurityConfig.kt
@@ -1,20 +1,42 @@
 package com.zera.store.security
 
+import com.zera.store.account.service.AccountUserService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig {
+class WebSecurityConfig (val accountUserService: AccountUserService, val passwordEncoder: PasswordEncoder) {
+
     @Bean
-    fun webSecurityCustomizer(): WebSecurityCustomizer? {
-        return WebSecurityCustomizer { web: WebSecurity ->
-            web.ignoring()
-                .anyRequest()
-        }
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { crsf -> crsf.disable() }
+            .authorizeHttpRequests { authorize -> authorize
+                .requestMatchers("/account/register")
+                    .permitAll()
+                .requestMatchers("/account/admin**").hasAuthority("ADMIN")
+                .requestMatchers("/product**").hasAnyAuthority("ADMIN", "CUSTOMER")
+                    .anyRequest().authenticated()
+            }
+            .formLogin(withDefaults())
+            .httpBasic(withDefaults())
+        return http.build()
     }
+
+    @Bean
+    fun daoAuthenticationProvider(): DaoAuthenticationProvider? {
+        val provider = DaoAuthenticationProvider()
+        provider.setPasswordEncoder(passwordEncoder)
+        provider.setUserDetailsService(accountUserService)
+        return provider
+    }
+
 }
